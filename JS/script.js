18166b6f00ba085e9c09db77a9fa30edc50b16b7
OliverJorgensen <43332915+Olli4000@users.noreply.github.com>
2019-01-18 07:48:22 +0100

    Ændrede sådan at mine links are korrekt

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var myNumb = 10; // gemmer et tal i en Variable
+    var countElement = ""; // gemmer en tom string in en Variable
+    console.log(myNumb); // udskriver min Variable myNumb 
+
+    document.querySelector("#buttonPlus").addEventListener({
+    myNumb++; // Tæller op
+    if (myNumb > 15) { // hvis variablen er mindre ind 15 så
+        myNumb = 11;
+    } //slutter funktionen
+    
+}) 
+    function updateCounter (){ //
+        countElement; //
+    }
+});
+
+
+/*
+Functions
+Loop, ForEach, While
+Conditions
+*/
